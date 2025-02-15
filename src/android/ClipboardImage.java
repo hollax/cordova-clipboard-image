b2b3777edaf31935dc76e581bb6a122a30c62b24
Wakeel Ogunsanya <wakeel.ogun@gmail.com>
2025-02-15 09:16:35 +0100

    Return file mime type

--- src/android/ClipboardImage.java
+++ src/android/ClipboardImage.java
@@ -8,15 +8,15 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.webkit.MimeTypeMap;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ClipboardImage extends CordovaPlugin {
@@ -37,16 +37,24 @@ public class ClipboardImage extends CordovaPlugin {
             Uri uri = clipboard.getPrimaryClip().getItemAt(0).getUri();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(cordova.getActivity().getContentResolver(), uri);
+                
+                // Convert Bitmap to BitmapDrawable if needed
+                BitmapDrawable bitmapDrawable = new BitmapDrawable(cordova.getActivity().getResources(), bitmap);
 
-                // Option 1: Return Base64 String
+                // Convert bitmap to Base64
                 String base64Image = convertBitmapToBase64(bitmap);
-                callbackContext.success(base64Image);
 
-                // Option 2: Save Image as File and Return Path (Uncomment if needed)
-                // String imagePath = saveBitmapToFile(bitmap);
-                // callbackContext.success(imagePath);
+                // Get MIME type
+                String mimeType = getMimeType(uri);
 
-            } catch (IOException e) {
+                // Prepare JSON response
+                JSONObject response = new JSONObject();
+                response.put("base64", base64Image);
+                response.put("mimeType", mimeType);
+
+                callbackContext.success(response);
+
+            } catch (IOException | JSONException e) {
                 callbackContext.error("Failed to retrieve image from clipboard.");
             }
         } else {
@@ -61,12 +69,12 @@ public class ClipboardImage extends CordovaPlugin {
         return Base64.encodeToString(byteArray, Base64.DEFAULT);
     }
 
-    private String saveBitmapToFile(Bitmap bitmap) throws IOException {
-        File file = new File(cordova.getActivity().getCacheDir(), "clipboard_image.png");
-        FileOutputStream fos = new FileOutputStream(file);
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-        fos.flush();
-        fos.close();
-        return file.getAbsolutePath();
+    private String getMimeType(Uri uri) {
+        String type = cordova.getActivity().getContentResolver().getType(uri);
+        if (type == null) {
+            String extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+        }
+        return type != null ? type : "image/png"; // Default to PNG if unknown
     }
 }
